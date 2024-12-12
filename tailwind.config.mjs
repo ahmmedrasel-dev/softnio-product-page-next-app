ca5bcba39a979ba2a599b94ba015bf45c00a294c
ahmmedrasel-dev <rahmmed.info@gmail.com>
2024-12-12 22:52:53 +0600

    create product object and install react icons

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,9 +8,10 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      extend: {
+        container: {
+          center: true,
+        },
       },
     },
   },
